8629bd57380900e3b023ee51fdef02f98c341157
Gregory Kramida <gregory.kramida@presagetech.com>
2025-08-18 17:20:10 -0400

    Update security & package list generation script

--- update_package_info.sh
+++ update_package_info.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 set -x
+set -e
 
 rm -f Packages Release Packages.gz InRelease
 dpkg-scanpackages --multiversion . > Packages
 gzip -k -f Packages
 apt-ftparchive release . > Release
-gpg --default-key "maintainer@presagetech.com" -abs -o - Release > Release.gpg
-gpg --default-key "maintainer@presagetech.com" --clearsign -o - Release > InRelease
+gpg --default-key "maintainer@presagetech.com" --digest-algo SHA512 -abs -o - Release > Release.gpg
+gpg --default-key "maintainer@presagetech.com" --digest-algo SHA512 --clearsign -o - Release > InRelease
